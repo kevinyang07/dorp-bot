5e2acb0d918a72480a94039087589afa54d514b5
kevinyang07 <69771499+kevinyang07@users.noreply.github.com>
2020-11-19 20:51:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -107,20 +107,6 @@ async def shutdown(ctx):
     else:
         await ctx.send("Death is whoever does Death’s job: me")
 
-@client.command(name='Search', description='Google searches and sends the top results to text channel, default # of results are one but can be changed in request', help='Type "Search" and an optional amount of desired results followed by a google search query')
-async def LetMeGoogleThat(ctx, results=1):
-    if results > 100:
-        await ctx.send("Too many results to search for try 100 or less")
-        return
-
-    await ctx.send('Please enter search Query')
-    query = await client.wait_for('message')
-
-    if query.author != client:
-        await ctx.send(f'now searching for {results} results for "{query.content}"')
-        for i in search(str(query.content), stop=results, verify_ssl=False, safe='on'):
-            await ctx.send(i)
-
 @client.command(pass_context=True)
 async def poll(ctx, question, arg, *, arg2=30):
 
